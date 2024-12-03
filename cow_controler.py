0d9b6ea4a72109528753a10c719afbf1c1d934ce
Nguyen Truong Than <truongthanxlscpy@gmail.com>
2024-12-03 23:42:36 +0700

    update import CowModel

--- cow_controler.py
+++ cow_controler.py
@@ -1,12 +1,15 @@
-import CowModel
+from CowModel import CowModel
+import threading
+
+cows_mutex = threading.Lock()
 cows_dict = {}
 
-def add_cow(cowModel: CowModel.CowModel):
+def add_cow(cowModel: CowModel):
     cows_dict[cowModel.cow_id] = cowModel
 
 def delete_cow(cow_id: str):
     del cows_dict[cow_id]
 
-def update_cow(cowModel: CowModel.CowModel):
+def update_cow(cowModel: CowModel):
     cows_dict[cowModel.cow_id] = cowModel
 
