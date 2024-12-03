df5d6bfd24b455cd003a9c63069143a7c9d0b29c
Nguyen Truong Than <truongthanxlscpy@gmail.com>
2024-12-03 23:41:26 +0700

    update read lora data from uart

--- src/thread/read_lora_thread.cpp
+++ src/thread/read_lora_thread.cpp
@@ -7,38 +7,48 @@ extern "C" {
 #include "global.hpp"
 #include "lora.hpp"
 
-
 void read_lora_entry(void *p1, void *p2, void *p3)
 {
     if(init_lora()){
         return;
     }
     uint8_t data;
-    uint8_t buffer[512] = {0};
-    uint8_t index = 0, is_begin = 0;
+    uint8_t lora_buffer[128] = {0};
+    uint8_t length = 0, is_begin = 0;
+    uint8_t lora_send_buffer[50] = {0};
 	while (1) {
         while(lora_read(&data) == 0){
-            /* ------------ Test ------------- */
-            std::cout << data;
-            if(data == '#') {
-                std::cout << std::endl;
-            }
-            /* -------------------------------- */
-            
-            if(is_begin == 1){
-                if(data == '#'){
-                    handle_lora_message(buffer, index);
-                    index = 0, is_begin = 0;
-                }else{
-                    buffer[index ++] = data;
-                }
+
+            if(data == '!'){
+                is_begin = 1;
             }else{
-                if(data == '!'){
-                    is_begin = 1, index = 0;
+                if(is_begin == 1){
+                    if(data == '#') {
+                        // Handle data receive
+                        uint16_t cow_addr_recv = (uint16_t)lora_buffer[0] << 8 | (uint16_t)lora_buffer[1];
+                        std::cout << "cow_addr_recv: " << cow_addr_recv << std::endl;
+
+                        if(cow_addr_recv == cow_addr){
+                            std::cout << "handle lora recv\n";
+                            lora_send_buffer[0] = '!';              // 0
+                            lora_send_buffer[1] = cow_addr >> 8;    // 1
+                            lora_send_buffer[2] = cow_addr & 0xff;   // 2
+                            memcpy(lora_send_buffer + 3, &lat, 4);  // 3 4 5 6
+                            memcpy(lora_send_buffer + 7, &lng, 4);  // 7 8 9 10
+                            lora_send_buffer[11] = 0; // cow status // 11
+                            lora_send_buffer[12] = '#';             // 12
+
+                            lora_write(lora_send_buffer, 13);                            
+                        }
+                        is_begin = 0;
+                        length = 0;
+                    }else{
+                        lora_buffer[length++] = data;
+                    }
                 }
             }
+            
         }
-        
 		k_msleep(10);
 	}
 }
