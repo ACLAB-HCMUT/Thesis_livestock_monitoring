f6255c84b13a405d03eb4a8b83816244b5baeb0e
Nguyen Truong Than <truongthanxlscpy@gmail.com>
2024-12-30 10:13:15 +0700

    Update header

--- src/controllers/mqttController.js
+++ src/controllers/mqttController.js
@@ -19,32 +19,30 @@ const handle_mqtt_msg = async (topic, msg) => {
             response_msg = "0" + constants.HEADER_BACKEND_RESPONSE_GET_COWS + JSON.stringify(cows);
             mqttService.publish(topic, response_msg);
             break;
-        case constants.HEADER_GATEWAY_SEND_COW_STATUS:
-            /* Call service update cow status */
-            console.log("HEADER_GATEWAY_SEND_COW_STATUS");
+        // case constants.HEADER_GATEWAY_SEND_COW_STATUS:
+        //     /* Call service update cow status */
+        //     console.log("HEADER_GATEWAY_SEND_COW_STATUS");
+        //     data = msg.slice(2);
+        //     split_data = data.split(':');
+        //     cow_id = split_data[0];
+        //     let cow_status = split_data[1];
+        //     await cowService.updateCowStatusById(cow_id, cow_status);
+        //     break;
+        case constants.HEADER_GATEWAY_SEND_COW_INFOR:
             data = msg.slice(2);
             split_data = data.split(':');
-            cow_id = split_data[0];
-            let cow_status = split_data[1];
-            await cowService.updateCowStatusById(cow_id, cow_status);
-            break;
-        case constants.HEADER_GATEWAY_SEND_GPS:
-            console.log("HEADER_GATEWAY_SEND_GPS");
-            data = msg.slice(2);
-            split_data = data.split(':');
+            console.log("HEADER_GATEWAY_SEND_COW_INFOR", split_data);
             cow_id = split_data[0];
             let longitude = parseFloat(split_data[1]);
             let latitude = parseFloat(split_data[2]);
-            
+            let is_out_safezone = parseInt(split_data[3]);
             /* Call service update latest gps */
             await cowService.updateLatestLocationById(cow_id, longitude, latitude);
             /* Call service create new gps */
             await cowLocationService.createCowLocation(cow_id, longitude, latitude);
-
-            if(split_data.length == 4) {
-                /* Cow out of safe area */
-                /* Publish notification to user */
-
+            if(is_out_safezone) {
+                /*  Call service to infor out of safezone to user by Firebase */
+                
             }
             break;
         case constants.HEADER_GATEWAY_REQUEST_SAFE_ZONES:
@@ -61,7 +59,7 @@ const handle_mqtt_msg = async (topic, msg) => {
             break;
 
         default:
-            console.log("Header invalid: ", header);
+            // console.log("Header invalid: ", header);
             break;
     }
 }
